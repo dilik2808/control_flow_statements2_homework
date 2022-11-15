c4c5f012659cd83815d53fd0149977ae89293ee9
Dilshod <dilik2808@gmail.com>
2022-11-15 17:03:05 +0500

    5

--- if05.py
+++ if05.py
@@ -6,4 +6,29 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1= n%10
+    x2= n%100//10
+    x3= n%1000//100
+    x4= n%10000//1000
+    x5= n//10000
+    if x1>x2:
+        if x1>x3:
+            if x1>x4:
+                if x1>x5:
+                    return x1
+                else: return x5
+
+    if x2>x3:
+        if x2>x4:
+            if x2>x5:
+                return x2
+            else: return x5
+    if x3>x4:
+        if x3>x5:
+            return x3
+        else: return x5
+    if x4>x5:
+        return x4
+    else: return x5
+
+print (main(82795))
